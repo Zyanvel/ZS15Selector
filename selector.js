39c529f40ea2106cab48a311c38b7a41868bcc0c
Zyanvel <zyanvel@gmail.com>
2020-10-18 18:05:59 -0700

    first commit

---
+++ selector.js
@@ -0,0 +1,6 @@
+document.getElementById("first");
+document.getElementsByClassName("special")[0];
+document.querySelector("#first");
+document.querySelectorAll(".special")[0];
+document.getElementsByTagName("p")[0];
+document.querySelector(".special");
